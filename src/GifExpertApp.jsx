9fd9e37185b497a3cff1f87a6e48ff57f13760ee
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-04-05 21:57:23 -0600

    App estable

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,25 +1,20 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState(["One Punch"]);
+    const [categories, setCategories] = useState([]);
     const onAddCategory = (category) => {
+        if (categories.includes(category)) return; // Evita duplicados
         setCategories([category, ...categories]);
     };
     return (
         <>
             <h1>GifExpertApp</h1>
-            {/* input */}
             <AddCategory onAddCategory={onAddCategory} />
-
-            {/* listado de gif */}
-            <ol>
-                {categories.map((category) => {
-                    return <li key={category}>{category}</li>;
-                })}
-            </ol>
-
-            {/* gif item */}
+            {categories.map((category) => {
+                return <GifGrid key={category} category={category} />;
+            })}
         </>
     );
 };
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 
 export const AddCategory = ({ onAddCategory }) => {
     const [inputValue, setInputValue] = useState("");
+
     const onChangeInput = (e) => {
         setInputValue(e.target.value);
     };
     const onSubmit = (e) => {
         e.preventDefault(); // Evita que se recargue la página 
         if (inputValue.trim().length <= 1) return; // Evita que se agregue un valor vacío
-        onAddCategory(inputValue); // Llama a la función onAddCategory de GifExpertApp
+        onAddCategory(inputValue.trim()); // Llama a la función onAddCategory de GifExpertApp
         setInputValue(""); // Limpia el input
     };
 
